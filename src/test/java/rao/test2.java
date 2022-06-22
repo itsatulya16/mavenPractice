400a4efe68e45e58e91833989754e4d037f4c0eb
Lenovo <Lenovo@Atul>
2022-06-22 08:32:43 +0530

    hhhhhhhhhhhhhhhhhhh

---
+++ src/test/java/rao/test2.java
@@ -0,0 +1,11 @@
+package rao;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	void mama() {
+		System.out.println("babuji");
+	}
+
+}
--- src/test/java/rao/ttest.java
+++ src/test/java/rao/ttest.java
@@ -1,9 +1,13 @@
 package rao;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import babu.login;
@@ -17,19 +21,29 @@ public class ttest {
 System.setProperty("webdriver.chrome.driver","C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver1.exe");
 		 driver=new ChromeDriver();
 		 driver.manage().window().maximize();
-		 driver.get("https://www.facebook.com/login/web/");
+		 
 		 l=new login(driver);
+		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
- @Test (priority=0)
+	@BeforeMethod
+	void launchbaby() {
+		driver.get("https://www.facebook.com/login/web/");
+	}
+ @Test (invocationCount=2,priority=0)
 	void logintest() {
 		l.signup();
-		String exp="https://www.facebook.com/r.php?locale=en_GB&display=page";
-		Assert.assertEquals(driver.getCurrentUrl(),exp );
+	//	String exp="https://www.facebook.com/r.php?locale=en_GB&display=page";
+		//Assert.assertEquals(driver.getCurrentUrl(),exp );
 		
 	}
- @Test(priority=1)
+ @Test(priority=1, dependsOnMethods="logintest")
  void backtest() {
 	 driver.navigate().back();
+	 
+ }
+ @AfterMethod
+ void closeitbabes() {
+	 driver.close();
  }
 	
 	
